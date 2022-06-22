e5d4820e7787279c8188c32555db602bbe5c72a0
MattT <MattT@DESKTOP-SKC2NV7>
2022-06-21 21:12:42 -0500

    Add project files.

---
+++ CityInfo.API/Controllers/FilesControllers.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesControllers : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesControllers(
+            FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+           _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new System.ArgumentNullException(
+                    nameof(fileExtensionContentTypeProvider));
+        }
+        [HttpGet("{fileId")]
+        public ActionResult GetFile(string fileId)
+        {
+            // look up the actual file, depnding on the fileId...
+            // demo code
+            var pathToFile = "demo";
+
+            // check whether the file exists
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+            
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(
+                pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllText(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,41 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterst")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfinterst(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city.PointsOfInterest);
+        }
+        [HttpGet("{pointofinterest}")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterstId)
+        {
+            var city = CitiesDataStore.Current.Cities
+                 .FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            // find point of interest
+            var pointOfInterest = city.PointsOfInterest
+                .FirstOrDefault(c => c.Id == pointOfInterstId);
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            return Ok(pointOfInterest);
+        }
+    }
+}
